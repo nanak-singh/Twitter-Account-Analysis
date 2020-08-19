613f740631bdfe55f8e294871882a395dbe112af
nanak-singh <39913517+nanak-singh@users.noreply.github.com>
2020-08-19 22:25:49 +0530

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# Go to https://apps.twitter.com/ and create an app.
+# The consumer key and secret will be generated for you
+consumer_key="###"
+consumer_secret="###"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Create New App" section
+access_token="###"
+access_token_secret="###"
